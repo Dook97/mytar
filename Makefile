d5ee9a7307407f14e138a712d9b28131b4b6bff0
Jan Doskočil <30779172+Dook97@users.noreply.github.com>
2022-05-20 00:00:00 +0000

    fix argument parsing function

--- Makefile
+++ Makefile
@@ -6,4 +6,4 @@ clean:
 	rm -f mytar *.o
 
 mytar: mytar.c
-	cc -Wall -Wextra -o mytar mytar.c
+	gcc -Wall -Wextra -std=c99 -o mytar mytar.c
--- mytar.c
+++ mytar.c
@@ -6,6 +6,10 @@
 
 #define MIN(a,b) (((a) < (b)) ? (a) : (b))
 #define MAX(a,b) (((a) > (b)) ? (a) : (b))
+#define LEN(arr) (sizeof(arr) / sizeof(arr[0]))
+
+#define ERR(ERR, FORMAT, ...) errx(ERR, "err (%d): " FORMAT, ERR, ## __VA_ARGS__)
+#define WARN(ERR, FORMAT, ...) warnx(ERR, "warn (%d): " FORMAT, ERR, ## __VA_ARGS__)
 
 /* exit codes */
 #define NO_ACTION		2
@@ -17,62 +21,65 @@ typedef struct {
 	char operation; // t, x or 0 for invalid
 	char **files;
 	int file_count;
-} job_t;
+	char *archive_file;
+} args;
 
-void err_(int code, char *msg) {
-	errx(code, "err (%d): %s", code, msg);
-}
-
-job_t get_args(int argc, char **argv) {
-	job_t out = {
+args get_args(int argc, char **argv) {
+	args out = {
 		.operation = 0,
 		.files = (char**)(malloc((argc - 1) * sizeof(char*))),
-		.file_count = 0
+		.file_count = 0,
+		.archive_file = NULL
 	};
 
 	char **argv_ = argv;
-	bool filemode = false;
 	while (*++argv_) {
 		char *arg = *argv_;
 		if (arg[0] == '-') {
 			switch (arg[1]) {
-				case 't':
 				case 'x':
+				case 't':
 					if (out.operation != 0)
-						err_(MULTIPLE_ACTIONS, "A single action (-t or -x) must be specified.");
+						ERR(MULTIPLE_ACTIONS, "A single action (-t or -x) must be specified.");
 					out.operation = arg[1];
 					break;
+
 				case 'f':
-					filemode = true;
+					if (*++argv_)
+						arg = *argv_;
+					if (arg[0] == '-')
+						ERR(MISSING_ARGUMENT, "Expected archive name following \'-f\'.");
+					size_t archive_size = (strlen(arg) + 1) * sizeof(char);
+					out.archive_file = (char*)(malloc(archive_size));
+					strcpy(out.archive_file, *argv_);
 					break;
+
 				default:
-					errx(INVALID_OPTION, "err (%d): No such option \"%s\".", INVALID_OPTION, arg);
-					break;
+					ERR(INVALID_OPTION, "No such option \"%s\".", arg);
 			}
-		}
-		else {
-			if (filemode) {
-				size_t filename_size = strlen(arg) * sizeof(char);
-				*(out.files + out.file_count) = (char*)(malloc(filename_size));
-				strcpy(*(out.files + out.file_count), *argv_);
-				++(out.file_count);
-			} else {
-				err_(MISSING_ARGUMENT, "-f was expected before filename.");
-			}
+		} else {
+			size_t filename_size = (strlen(arg) + 1) * sizeof(char);
+			out.files[out.file_count] = (char*)(malloc(filename_size));
+			strcpy(out.files[out.file_count], *argv_);
+			++(out.file_count);
 		}
 	}
 
 	if (!out.operation)
-		err_(NO_ACTION, "Expected -x or -t but neither was given.");
+		ERR(NO_ACTION, "Expected -x or -t but neither was given.");
+	if (!out.archive_file)
+		ERR(MISSING_ARGUMENT, "Expected \'-f ARCHIVE_NAME\'.");
 
 	return out;
 }
 
 int main(int argc, char **argv) {
-	job_t job = get_args(argc, argv);
+	args job = get_args(argc, argv);
+
+	printf("archive name: %s\n", job.archive_file);
 	printf("action: %c\n", job.operation);
 	printf("file count: %d\n", job.file_count);
-	printf("filename: ");
+	printf("files: ");
 	for (int i = 0; i < job.file_count; ++i) {
 		printf("%s ", job.files[i]);
 	}
